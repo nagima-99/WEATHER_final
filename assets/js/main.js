2c232a80b3126ba2a4c7aecee179e77e802e3d6b
stick334 <donottookey@protonmail.com>
2024-12-15 11:49:45 +0600

    map update 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,14 @@
 
 
-// fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
-//     .then((response) => {
-//         return response.json();
-//     })
-//     .then((data) => {
-//         console.log(data);
-//     });
+fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
 
 
-// const map = L.map('map').setView([51.16, 71.43], 10); // Координаты поменять не те взяты 
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-
-// // Получение погоды
-// const WEATHER_API_KEY = 'YOUR_WEATHER_API_KEY'; // Скачать ключ OpenWeatherMap
-// navigator.geolocation.getCurrentPosition(async (position) => {
-//     const { latitude, longitude } = position.coords;
-//     const response = await fetch("
-//         https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_API_KEY}
-//     ")
-//     const data = await response.json();
-//     document.getElementById('weather-data').innerText =
-//         Temperature: ${data.main.temp}°C, ${data.weather[0].description};
-// });
-
-// // Получение курсов валют вроде тот код
-// const CURRENCY_API_URL = 'https://open.er-api.com/v6/latest/USD'; // Пример API
-// fetch(CURRENCY_API_URL)
-//     .then((response) => response.json())
-//     .then((data) => {
-//         const fromSelect = document.getElementById('from-currency');
-//         const toSelect = document.getElementById('to-currency');
-//         Object.keys(data.rates).forEach((currency) => {
-//             fromSelect.innerHTML += <option value="${currency}">${currency}</option>;
-//             toSelect.innerHTML += <option value="${currency}">${currency}</option>;
-//         });
-//         document.getElementById('currency-form').addEventListener('submit', (e) => {
-//             e.preventDefault();
-//             const amount = document.getElementById('amount').value;
-//             const from = fromSelect.value;
-//             const to = toSelect.value;
-//             const result = (amount / data.rates[from]) * data.rates[to];
-//             document.getElementById('conversion-result').innerText = 
-//                 ${amount} ${from} = ${result.toFixed(2)} ${to};
-//         });
-//     });
-
+async function getWeatherTemp(params) {
+    
+}
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,20 +1,216 @@
-
 initMap();
 
-        async function initMap() {
-            await ymaps3.ready;
+async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
 
-            const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
 
-            const map = new YMap(
-                document.getElementById('map'),
-                {
-                    location: {
-                        center: [37.588144, 55.733842],
-                        zoom: 10
-                    }
-                }
-            );
+    // Иницилиазируем карту
+    const map = new YMap(
+        // Передаём ссылку на HTMLElement контейнера
+        document.getElementById('map'),
 
-            map.addChild(new YMapDefaultSchemeLayer());
+        // Передаём параметры инициализации карты
+        {
+            location: {
+                // Координаты центра карты
+                center: [100.28133603255901,69.36563991583309],
+
+                // Уровень масштабирования
+                zoom: 10
+            }
         }
+    );
+
+    // Добавляем слой для отображения схематической карты
+    map.addChild(new YMapDefaultSchemeLayer());
+
+    map.addChild(new YMapDefaultSchemeLayer({
+        theme: "dark", 
+        customization: [
+            {
+                "tags": {
+                    "any": [
+                        "urban_area",
+                        "locality"
+                    ]
+                },
+                "elements": "geometry",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "terrain",
+                        "bathymetry",
+                        "landscape"
+                    ],
+                    "none": "land"
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "park",
+                        "cemetery"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "vegetation"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "industrial",
+                        "construction_site",
+                        "medical",
+                        "sports_ground",
+                        "beach"
+                    ]
+                },
+                "types": "polygon",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": "transit",
+                    "none": [
+                        "transit_location",
+                        "transit_line",
+                        "transit_schema",
+                        "is_unclassified_transit"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": "urban_area",
+                    "none": [
+                        "residential",
+                        "industrial",
+                        "cemetery",
+                        "park",
+                        "medical",
+                        "sports_ground",
+                        "beach",
+                        "construction_site"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": "poi",
+                    "none": [
+                        "outdoor",
+                        "park",
+                        "cemetery",
+                        "medical"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": "road"
+                },
+                "types": "point",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "food_and_drink",
+                        "shopping",
+                        "commercial_services"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "traffic_light"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "entrance"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road"
+                    ],
+                    "none": [
+                        "road_1",
+                        "road_2",
+                        "road_3",
+                        "road_4",
+                        "road_5",
+                        "road_6",
+                        "road_7"
+                    ]
+                },
+                "elements": "label.icon",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "building",
+                        "address",
+                        "fence"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            }
+        ]
+    }));
+}
+
+
